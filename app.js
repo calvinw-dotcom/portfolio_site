04fc8c2541fa14bd5c493ef638d8477b6724950a
calvinw-dotcom <79317167+calvinw-dotcom@users.noreply.github.com>
2025-05-28 10:20:24 +0100

    tidying comments

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-// const hiddenContent = document.querySelector('.hidden-content');
-// const readMore = document.querySelector('.read-more');
-
-// function revealContent(){
-//     hiddenContent.classList.toggle('reveal-button');
-// }
-
-// readMore.addEventListener('click', revealContent);
 
 document.querySelector('.projects').addEventListener('click',function(e){ //listens for all clicks on parent container
     const target = e.target;
@@ -18,10 +10,3 @@ document.querySelector('.projects').addEventListener('click',function(e){ //list
 
 })
 
-
-    // document.querySelector('#id of parent element').addEventListener('click',function(e){
-    //     const target = e.target;
-    //     if (target.mathes('CSS selector')){
-    //         target.style.backgroundColor = 'lightgray';
-    //     }
-    
