cb4a189acd2df3306f09258f0f400904ae7785b2
calvinw-dotcom <79317167+calvinw-dotcom@users.noreply.github.com>
2025-06-13 09:40:53 +0100

    Add pics and fixx "read more" bug

--- app.js
+++ app.js
@@ -10,3 +10,13 @@ document.querySelector('.projects').addEventListener('click',function(e){ //list
 
 })
 
+document.querySelector('.contributions').addEventListener('click',function(e){ //listens for all clicks on parent container
+    const target = e.target;
+    if (target.matches('.read-more')){
+        const hiddenContent = target.previousElementSibling;
+        const isVisible = hiddenContent.classList.toggle('reveal-button');
+        target.textContent = isVisible ? 'read less':'read more'
+
+    }
+
+})
